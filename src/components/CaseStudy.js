4963767f7bbcb410621c3cebc92c75d4b41b1188
Eyel Mordido <eyelmordido@gmail.com>
2022-05-01 23:36:02 -0400

    added info object to conditionally render case study properties

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -4,17 +4,25 @@ import '../styles/CaseStudy.scss';
 
 const CaseStudy = ({ caseStudy }) => {
 
+  const info = {
+    thumbnail: caseStudy.thumbnail || placeholder,
+    title: caseStudy.title || <i>Title not available</i>,
+    category: caseStudy.categories[0].title || <i>Category not available</i>,
+    excerpt: caseStudy.excerpt || <i>Excerpt not available</i>,
+    link: caseStudy.link || '#'
+  }
+
   return (
     <li className='case-study' data-aos='fade-up'>
       <figure className='case-study-img'>
-        <img src={caseStudy.thumbnail || placeholder} alt={caseStudy.title}/>
+        <img src={info.thumbnail} alt={info.title}/>
       </figure>
       <div className='case-study-details'>
-        <h2 className='case-study-category'>{caseStudy.categories[0].title}</h2>
-        <h3 className='case-study-title'>{caseStudy.title}</h3>
-        <figcaption className='case-study-text'>{caseStudy.excerpt}</figcaption>
+        <h2 className='case-study-category'>{info.category}</h2>
+        <h3 className='case-study-title'>{info.title}</h3>
+        <figcaption className='case-study-text'>{info.excerpt}</figcaption>
         <div className='case-study-link-container'>
-          <a href={caseStudy.link} className='case-study-link'>
+          <a href={info.link} className='case-study-link'>
             View case study
             <Arrow className='case-study-link-arrow'/>
           </a>
